164723e59ea85ae98141421c2a2178e6739cd867
宋志宸 <2451712@tongji.edu.cn>
2026-05-29 23:30:01 +0800

    refactor: 精简并规范化 main_visual.py 训练脚本
    移除冗余的自定义双流日志拦截器，交由外部系统或标准重定向处理。
    清理了训练循环中高频打印的时间戳、Batch Size 及 GPU 显存监控逻辑，降低吞吐开销。
    严格化参数解析，将 Mixup、标签平滑等关键策略改为 required=True，防止隐式默认值干扰实验。

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
 import argparse
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
+import math
 import os
 import sys
 import numpy as np
@@ -10,28 +11,15 @@ import time
 from model import *
 import torch.optim as optim 
 import random
+import pdb
+import shutil
 from LSR import LSR
 from torch.cuda.amp import autocast, GradScaler
-from datetime import datetime
 
-# ==================== 日志拦截器 ====================
-class DualStreamInterceptor(object):
-    def __init__(self, log_file_path):
-        self.terminal = sys.stdout
-        self.log_file = open(log_file_path, "a", encoding="utf-8")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log_file.write(message)
-        self.log_file.flush()
-
-    def flush(self):
-        self.terminal.flush()
-        self.log_file.flush()
-# ====================================================
 
 torch.backends.cudnn.benchmark = True
 parser = argparse.ArgumentParser()
+
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -47,42 +35,39 @@ parser.add_argument('--batch_size', type=int, required=True)
 parser.add_argument('--n_class', type=int, required=True)
 parser.add_argument('--num_workers', type=int, required=True)
 parser.add_argument('--max_epoch', type=int, required=True)
+parser.add_argument('--test', type=str2bool, required=True)
 
-parser.add_argument('--test', type=str2bool, required=False, default=False)
+# load opts
 parser.add_argument('--weights', type=str, required=False, default=None)
+
+# save prefix
 parser.add_argument('--save_prefix', type=str, required=True)
-parser.add_argument('--dataset', type=str, required=False, default='lrw1000')
-parser.add_argument('--border', type=str2bool, required=False, default=False)
-parser.add_argument('--mixup', type=str2bool, required=False, default=True)
-parser.add_argument('--label_smooth', type=str2bool, required=False, default=True)
-parser.add_argument('--se', type=str2bool, required=False, default=True)
+
+# dataset
+parser.add_argument('--dataset', type=str, required=True)
+parser.add_argument('--border', type=str2bool, required=True)
+parser.add_argument('--mixup', type=str2bool, required=True)
+parser.add_argument('--label_smooth', type=str2bool, required=True)
+parser.add_argument('--se', type=str2bool, required=True)
+
 
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
 
-# [修改点]: 日志文件将保存在专门的 logs 文件夹内，方便论文素材归档
-if not args.test:
-    log_dir = os.path.join(args.save_prefix, "logs")
-    os.makedirs(log_dir, exist_ok=True)
-    log_time = datetime.now().strftime('%Y%m%d_%H%M%S')
-    _ACADEMIC_LOG_FILE = os.path.join(log_dir, f"train_{log_time}.log")
-    sys.stdout = DualStreamInterceptor(_ACADEMIC_LOG_FILE)
-    print(f"Log stream system activated. Target path: {_ACADEMIC_LOG_FILE}")
-
-# [修改点]: 移除了 args.border = False 和 args.dataset_dir 的全局硬编码
-
 if(args.dataset == 'lrw'):
     from utils import LRWDataset as Dataset
 elif(args.dataset == 'lrw1000'):    
-    from utils.dataset_lrw1000 import LRW1000_Dataset as Dataset
+    from utils import LRW1000_Dataset as Dataset
 else:
     raise Exception('lrw or lrw1000')    
+
 
 video_model = VideoModel(args).cuda()
 
 def parallel_model(model):
     model = nn.DataParallel(model)
     return model        
+
 
 def load_missing(model, pretrained_dict):
     model_dict = model.state_dict()
@@ -94,20 +79,23 @@ def load_missing(model, pretrained_dict):
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
     return model
+    
 
 lr = args.batch_size / 32.0 / torch.cuda.device_count() * args.lr
 optim_video = optim.Adam(video_model.parameters(), lr = lr, weight_decay=1e-4)     
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optim_video, T_max = args.max_epoch, eta_min=5e-6)
 
+
 if(args.weights is not None):
     print('load weights')
     weight = torch.load(args.weights, map_location=torch.device('cpu'))    
     load_missing(video_model, weight.get('video_model'))
+    
           
 video_model = parallel_model(video_model)
 
 def dataset2dataloader(dataset, batch_size, num_workers, shuffle=True):
-    loader = DataLoader(dataset,
+    loader =  DataLoader(dataset,
             batch_size = batch_size, 
             num_workers = num_workers,   
             shuffle = shuffle,         
@@ -121,59 +109,53 @@ def add_msg(msg, k, v):
     msg = msg + k.format(v)
     return msg    
 
-def test(epoch=None):
+def test():
+    
     with torch.no_grad():
         dataset = Dataset('val', args)
-        print(f'\nStart Testing (Epoch {epoch}), Data Length: {len(dataset)}')
+        print('Start Testing, Data Length:',len(dataset))
         loader = dataset2dataloader(dataset, args.batch_size, args.num_workers, shuffle=False)        
         
+        print('start testing')
         v_acc = []
-        v_acc_top5 = [] 
+        entropy = []
+        acc_mean = []
         total = 0
+        cons_acc = 0.0
+        cons_total = 0.0
+        attns = []
 
         for (i_iter, input) in enumerate(loader):
+            
             video_model.eval()
             
             tic = time.time()
             video = input.get('video').cuda(non_blocking=True)
             label = input.get('label').cuda(non_blocking=True)
             total = total + video.size(0)
+            names = input.get('name')
             border = input.get('duration').cuda(non_blocking=True).float()
             
             with autocast():
                 if(args.border):
-                    y_v = video_model(video, border)                                            
+                    y_v = video_model(video, border)                                           
                 else:
-                    y_v = video_model(video)                                            
-            
+                    y_v = video_model(video)                                           
+                                
+
             v_acc.extend((y_v.argmax(-1) == label).cpu().numpy().tolist())
-            
-            _, top5_preds = y_v.topk(5, dim=-1)
-            top5_correct = top5_preds.eq(label.view(-1, 1).expand_as(top5_preds))
-            v_acc_top5.extend(top5_correct.sum(dim=-1).clamp(max=1).cpu().numpy().tolist())
-            
             toc = time.time()
             if(i_iter % 10 == 0):  
                 msg = ''              
-                msg = add_msg(msg, 'v_acc_top1={:.5f}', np.array(v_acc).reshape(-1).mean())                
-                msg = add_msg(msg, 'v_acc_top5={:.5f}', np.array(v_acc_top5).reshape(-1).mean())                
+                msg = add_msg(msg, 'v_acc={:.5f}', np.array(v_acc).reshape(-1).mean())                
                 msg = add_msg(msg, 'eta={:.5f}', (toc-tic)*(len(loader)-i_iter)/3600.0)
+                                
                 print(msg)            
 
         acc = float(np.array(v_acc).reshape(-1).mean())
-        acc_top5 = float(np.array(v_acc_top5).reshape(-1).mean())
+        msg = 'v_acc_{:.5f}_'.format(acc)
         
-        exam_report = f"""
-==================================================
-        [Epoch {epoch} Final Exam Results]
-        Top-1 Accuracy : {acc:.5f}
-        Top-5 Accuracy : {acc_top5:.5f}
-==================================================
-        """
-        print(exam_report)
-        
-        msg = 'v_acc_top1_{:.5f}_top5_{:.5f}_'.format(acc, acc_top5)
-        return acc, msg                                    
+        return acc, msg                                 
 
 def showLR(optimizer):
     lr = []
@@ -182,20 +164,33 @@ def showLR(optimizer):
     return ','.join(lr)
 
 def train():            
+    
+    
+    
     dataset = Dataset('train', args)
     print('Start Training, Data Length:',len(dataset))
     
     loader = dataset2dataloader(dataset, args.batch_size, args.num_workers)
         
     max_epoch = args.max_epoch    
+
+    
     ce = nn.CrossEntropyLoss()
 
     tot_iter = 0
     best_acc = 0.0
+    adjust_lr_count = 0
     alpha = 0.2
+    beta_distribution = torch.distributions.beta.Beta(alpha, alpha)
     scaler = GradScaler()             
-    
     for epoch in range(max_epoch):
+        total = 0.0
+        v_acc = 0.0
+        total = 0.0               
+        
+        lsr = LSR()
+        
+        
         for (i_iter, input) in enumerate(loader):
             tic = time.time()           
             
@@ -207,33 +202,36 @@ def train():
             loss = {}
             
             if(args.label_smooth):
-                loss_fn = LSR()
+                loss_fn = lsr
             else:
                 loss_fn = nn.CrossEntropyLoss()
             
             with autocast():
                 if(args.mixup):
-                    video_gpu = video.contiguous()
-                    
                     lambda_ = np.random.beta(alpha, alpha)
-                    index = torch.randperm(video_gpu.size(0)).cuda(non_blocking=True)
+                    index = torch.randperm(video.size(0)).cuda(non_blocking=True)
                     
-                    mix_video = lambda_ * video_gpu + (1 - lambda_) * video_gpu[index, :]
-                    label_a, label_b = label, label[index]            
-                    
-                    if(args.border):
-                        y_v = video_model(mix_video, border)
-                    else:
-                        y_v = video_model(mix_video)
+                    mix_video = lambda_ * video + (1 - lambda_) * video[index, :]
+                    mix_border = lambda_ * border + (1 - lambda_) * border[index, :]
                         
+                    label_a, label_b = label, label[index]            
+
+                    if(args.border):
+                        y_v = video_model(mix_video, mix_border)       
+                    else:                
+                        y_v = video_model(mix_video)       
+
                     loss_bp = lambda_ * loss_fn(y_v, label_a) + (1 - lambda_) * loss_fn(y_v, label_b)
+                    
                 else:
                     if(args.border):
-                        y_v = video_model(video, border)
-                    else:
-                        y_v = video_model(video)
+                        y_v = video_model(video, border)       
+                    else:                
+                        y_v = video_model(video)    
+                        
                     loss_bp = loss_fn(y_v, label)
                                     
+            
             loss['CE V'] = loss_bp
                 
             optim_video.zero_grad()   
@@ -242,34 +240,30 @@ def train():
             scaler.update()
             
             toc = time.time()
-            time_str = datetime.now().strftime('%H:%M:%S')
-            current_batch_size = video.size(0)
             
-            if torch.cuda.is_available():
-                gpu_mem_allocated = torch.cuda.memory_allocated() / (1024 ** 3)
-                gpu_mem_reserved = torch.cuda.memory_reserved() / (1024 ** 3)
-            else:
-                gpu_mem_allocated = 0.0
-                gpu_mem_reserved = 0.0
-            
-            msg = 'time={},epoch={},step={:04d},train_iter={},eta={:.5f}'.format(time_str, epoch, i_iter, tot_iter, (toc-tic)*(len(loader)-i_iter)/3600.0)
+            msg = 'epoch={},train_iter={},eta={:.5f}'.format(epoch, tot_iter, (toc-tic)*(len(loader)-i_iter)/3600.0)
             for k, v in loss.items():                                                
                 msg += ',{}={:.5f}'.format(k, v)
             msg = msg + str(',lr=' + str(showLR(optim_video)))                    
             msg = msg + str(',best_acc={:2f}'.format(best_acc))
-            msg = msg + f',batch_size={current_batch_size},mixup={args.mixup},gpu_alloc={gpu_mem_allocated:.2f}GB,gpu_reserv={gpu_mem_reserved:.2f}GB'
             print(msg)                                
             
             if(i_iter == len(loader) - 1 or (epoch == 0 and i_iter == 0)):
-                acc, msg_test = test(epoch=epoch)
+
+                acc, msg = test()
 
                 if(acc > best_acc):
-                    savename = '{}_iter_{}_epoch_{}_{}.pt'.format(args.save_prefix, tot_iter, epoch, msg_test)
+                    savename = '{}_iter_{}_epoch_{}_{}.pt'.format(args.save_prefix, tot_iter, epoch, msg)
+                    
                     temp = os.path.split(savename)[0]
                     if(not os.path.exists(temp)):
                         os.makedirs(temp)                    
-                    torch.save({'video_model': video_model.module.state_dict()}, savename)         
+                    torch.save(
+                        {
+                            'video_model': video_model.module.state_dict(),
+                        }, savename)         
                      
+
                 if(tot_iter != 0):
                     best_acc = max(acc, best_acc)    
                     
@@ -279,7 +273,7 @@ def train():
         
 if(__name__ == '__main__'):
     if(args.test):
-        acc, msg = test(epoch="Test Mode")
-        print(f'Final Test Acc = {acc}')
+        acc, msg = test()
+        print(f'acc={acc}')
         exit()
     train()
